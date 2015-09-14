52dbfc1cd256863a41676874861c7e24ad92c438
RufusWong <rcywongaa@gmail.com>
2015-09-14 20:59:22 +0800

    OpenGL: Draw triangle

--- inc/cv_commons.hpp
+++ inc/cv_commons.hpp
@@ -3,7 +3,12 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
+#include <GL/glew.h>
+#define GLFW_INCLUDE_GLU
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 #include <iostream>
+#include <fstream>
 
 #include "config.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,94 @@
 #include "cv_commons.hpp"
 
+#define HEIGHT 480
+#define WIDTH 640
+#define VERTEXSHADERFILE PROJECT_DIRECTORY "/src/VertexShader.cpp"
+#define FRAGMENTSHADERFILE PROJECT_DIRECTORY "/src/FragmentShader.cpp"
+
 using namespace cv;
+using namespace glm;
 using namespace std;
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
+
+    // Create the shaders
+    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Read the Vertex Shader code from the file
+    std::string VertexShaderCode;
+    std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+    if(VertexShaderStream.is_open())
+    {
+        std::string Line = "";
+        while(getline(VertexShaderStream, Line))
+        VertexShaderCode += "\n" + Line;
+        VertexShaderStream.close();
+    }
+
+    // Read the Fragment Shader code from the file
+    std::string FragmentShaderCode;
+    std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+    if(FragmentShaderStream.is_open()){
+        std::string Line = "";
+        while(getline(FragmentShaderStream, Line))
+            FragmentShaderCode += "\n" + Line;
+        FragmentShaderStream.close();
+    }
+
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+
+    // Compile Vertex Shader
+    printf("Compiling shader : %s\n", vertex_file_path);
+    char const * VertexSourcePointer = VertexShaderCode.c_str();
+    glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
+    glCompileShader(VertexShaderID);
+
+    // Check Vertex Shader
+    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    std::vector<char> VertexShaderErrorMessage(InfoLogLength);
+    glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+    fprintf(stdout, "%s\n", &VertexShaderErrorMessage[0]);
+
+    // Compile Fragment Shader
+    printf("Compiling shader : %s\n", fragment_file_path);
+    char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
+    glCompileShader(FragmentShaderID);
+
+    // Check Fragment Shader
+    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    std::vector<char> FragmentShaderErrorMessage(InfoLogLength);
+    glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+    fprintf(stdout, "%s\n", &FragmentShaderErrorMessage[0]);
+
+    // Link the program
+    fprintf(stdout, "Linking program\n");
+    GLuint ProgramID = glCreateProgram();
+    glAttachShader(ProgramID, VertexShaderID);
+    glAttachShader(ProgramID, FragmentShaderID);
+    glLinkProgram(ProgramID);
+
+    // Check the program
+    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    std::vector<char> ProgramErrorMessage( std::max(InfoLogLength, int(1)) );
+    glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+    fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
+
+    //glDeleteShader(VertexShaderID);
+    //glDeleteShader(FragmentShaderID);
+
+    return ProgramID;
+}
 
 int main( int argc, char** argv )
 {
+    //cout << getBuildInformation() << endl;
+    GLenum err;
     Ptr<ORB> orbDetector;
     orbDetector = ORB::create();
     vector<KeyPoint> keypoints;
@@ -23,12 +107,70 @@ int main( int argc, char** argv )
         return -1;
     }
 
+    if ( !glfwInit() )
+    {
+        cout << "Failed to initialize GLFW" << endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "3D View", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if( window == NULL )
+    {
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+        glfwTerminate();
+        return -1;
+    }
+    //glfwSwapInterval(1);
+
+    glewExperimental = true; // Needed in core profile
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(glewInit()));
+        return -1;
+    }
+
+    //VertexArrayObject stores the VertexBufferObject and VertexAttribArray settings
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f
+    };
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    // 1st attribute buffer : vertices
+    glVertexAttribPointer(
+        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride
+        (void*)0            // array buffer offset
+    );
+
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    GLuint programID = LoadShaders( VERTEXSHADERFILE, FRAGMENTSHADERFILE );
+
     double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
     double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
 
     cout << "Frame size : " << dWidth << " x " << dHeight << endl;
 
-//    namedWindow("Keypoints",CV_WINDOW_AUTOSIZE);
+    //    namedWindow("Keypoints",CV_WINDOW_AUTOSIZE);
 
     Mat frame;
     Mat grayFrame;
@@ -37,6 +179,15 @@ int main( int argc, char** argv )
     vector<Rect> eyes;
     while (1)
     {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+        glEnableVertexAttribArray(0);
+        glBindVertexArray(VertexArrayID);
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 
         bool bSuccess = cap.read(frame); // read a new frame from video
 
@@ -53,15 +204,15 @@ int main( int argc, char** argv )
 
         for (int i = 0; i < faces.size(); i++)
         {
-          rectangle(faceFrame, faces[i], Scalar(255, 0, 0));
-          Mat faceROI = frame(faces[i]);
-          eyes_cascade.detectMultiScale(faceROI, eyes);
-          for (int j = 0; j < eyes.size(); j++)
-          {
-            Point faceCorner = faces[i].tl();
-            Rect eye = eyes[j];
-            rectangle(faceFrame, faceCorner + eye.tl(), faceCorner + eye.br(), Scalar(0, 255, 0));
-          }
+            rectangle(faceFrame, faces[i], Scalar(255, 0, 0));
+            Mat faceROI = frame(faces[i]);
+            eyes_cascade.detectMultiScale(faceROI, eyes);
+            for (int j = 0; j < eyes.size(); j++)
+            {
+                Point faceCorner = faces[i].tl();
+                Rect eye = eyes[j];
+                rectangle(faceFrame, faceCorner + eye.tl(), faceCorner + eye.br(), Scalar(0, 255, 0));
+            }
         }
 
         imshow("Face", faceFrame);
